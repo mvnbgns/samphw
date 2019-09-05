f3989f3e8e3d700db1c4a16b73b6be385b92ad3d
mvnbgns <31599832+mvnbgns@users.noreply.github.com>
2019-09-05 10:18:05 +0800

    Add files via upload

---
+++ marvin.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+echo "What is your name?: "
+read varname
+echo "Nice meeting you $varname"
+
+echo "Say 'hello'"
+while :
+do
+	read INPUT_STRING
+	case $INPUT_STRING in 
+			hello | Hello )
+			echo "Hello yourself"
+			;;
+		bye)
+			echo "see you again!"
+			break
+			;;
+
+		*)
+
+			echo "Sorry, I don't understand"
+			;;
+	esac
+done
+
