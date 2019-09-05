97475b9d6bb7d2a5be0ee76a5fc3197b3c82a8e8
mvnbgns <31599832+mvnbgns@users.noreply.github.com>
2019-09-05 10:02:06 +0800

    Update marvin.sh

--- marvin.sh
+++ marvin.sh
@@ -2,3 +2,20 @@
 echo "What is your name?: "
 read varname
 echo "Nice meeting you $varname"
+
+while:
+do
+    read INPUT_STRING
+    case $INPUT_STRING in
+    hello)
+            echo "Hello Yourself"
+            ;;
+    bye)
+            echo "See you again!"
+            break
+            ;;
+    *)
+            echo "Sorry, I don't understand"
+            ;;
+    esac
+done
